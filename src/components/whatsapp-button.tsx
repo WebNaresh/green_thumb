1adadf4cd7c7a1911db5179af427c72a48a0f4ac
bhos1242 <bhosvivek123@gmail.com>
2025-03-11 14:50:21 +0530

    Replace Whatsapp icon with an image and update button styles for better visibility

--- src/components/whatsapp-button.tsx
+++ src/components/whatsapp-button.tsx
@@ -1,15 +1,16 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { PhoneIcon as WhatsappIcon } from "lucide-react";
+
+import Image from "next/image";
 
 export function WhatsappButton() {
   return (
     <Button
-      className="fixed bottom-4 left-4 z-50 bg-green-500 hover:bg-green-600 text-white rounded-full shadow-lg"
+      className="fixed bottom-4 left-4 z-50 bg-white  text-green-500 rounded-full shadow-lg"
       size="lg"
       onClick={() => window.open("https://wa.me/919371202875", "_blank")}
     >
-      <WhatsappIcon className="mr-2 h-5 w-5" />
+      <Image src="/whatsapp.png" alt="Whatsapp Logo" width={24} height={24} />
       Chat with us
     </Button>
   );
